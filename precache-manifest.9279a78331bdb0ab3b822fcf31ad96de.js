cca232454ff1aa4c9090864a2e84eb6f41c03469
James Dorr <jamesdorr3@gmail.com>
2019-07-09 13:45:22 -0700

    Updates

---
+++ precache-manifest.9279a78331bdb0ab3b822fcf31ad96de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27115e7c5c95f8d2157539cce168f57e",
+    "url": "/tIPA/index.html"
+  },
+  {
+    "revision": "be002928593fb33279fe",
+    "url": "/tIPA/static/css/main.e4e4d609.chunk.css"
+  },
+  {
+    "revision": "9b0c6aea9890460feff5",
+    "url": "/tIPA/static/js/2.b13a9952.chunk.js"
+  },
+  {
+    "revision": "be002928593fb33279fe",
+    "url": "/tIPA/static/js/main.5ed90aa4.chunk.js"
+  },
+  {
+    "revision": "6dc20a09a44e4110010d",
+    "url": "/tIPA/static/js/runtime~main.6c9789b2.js"
+  }
+]);
